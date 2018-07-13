71de5718f8892836b0017c1a51e877a019752956
huang yingzheng <huangyingzheng123@gmail.com>
2018-07-13 18:04:59 +0200

    function test

--- server.js
+++ server.js
@@ -6,27 +6,6 @@ const { makeExecutableSchema } = require("graphql-tools");
 const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
 const app = express();
 
-
-// const _ = require("lodash")
-// const {normSchema,normResolver} = require("./norme.js")
-// const variable = require("./variable.js")
-// const dd = require(__dirname + "/default-engine/engine.js");
-//
-//
-// const jobResolver = dd.normalize(variable.entities[0], normResolver);
-// const companyResolver = dd.normalize(variable.entities[1], normResolver);
-// const queryResolver = dd.normalize(variable.entities[3], normResolver);
-// const mutationResolver = dd.normalize(variable.entities[4], normResolver);
-//
-// const resolvers = {
-//     Query: queryResolver.rf,
-//     Mutation: mutationResolver.rf,
-//     Job: jobResolver.srf,
-//     Company: companyResolver.srf
-// };
-// const schema1 = dd.normalize(variable.entities, normSchema);
-// // console.log(schema1)
-
 const run  = async (typeDefs,resolvers) => {
 
     const Mongo = await connectDataBase();
@@ -39,5 +18,4 @@ const run  = async (typeDefs,resolvers) => {
     app.listen(port, () => console.info(`Server started on port ${port}`));
 }
 
-// run(schema1,resolvers);
 module.exports = {run, app, connectDataBase}
